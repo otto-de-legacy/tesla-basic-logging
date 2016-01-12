5477293c6a9ecd3c1da5977a6ea0d23eab4d25ad
Tesla Working Station I (no comment) <tesla@dv.otto.de>
2016-01-12 13:31:43 +0100

    Bumped up clojure and dependency versions

--- project.clj
+++ project.clj
@@ -7,14 +7,14 @@
                       :url  "https://github.com/otto-de/tesla-basic-logging"}
 
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.slf4j/slf4j-api "1.7.12"]
+                 [org.slf4j/slf4j-api "1.7.13"]
                  [ch.qos.logback/logback-core "1.1.3"]
                  [ch.qos.logback/logback-classic "1.1.3"]
-                 [net.logstash.logback/logstash-logback-encoder "4.4"]
+                 [net.logstash.logback/logstash-logback-encoder "4.5.1"]
 
-                 [com.github.danielwegener/logback-kafka-appender "0.0.2"]]
+                 [com.github.danielwegener/logback-kafka-appender "0.0.5"]]
 
   :aot [de.otto.util.escapingmessageconverter]
 )
